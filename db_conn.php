e691c5157ce3b874101cbf09b0166848ee174b2f
Koala10k <dupeng0217@gmail.com>
2014-04-24 01:05:06 +1000

    simplize menu.css

--- db_conn.php
+++ db_conn.php
@@ -3,32 +3,10 @@
 // ob_start();
 // $firephp = FirePHP::getInstance(true);
 // $firephp -> log('starting ');
-// $session = ssh2_connect('131.217.36.2', 22);
-// $firephp -> log('Session ');
-// if (ssh2_auth_password($session, 'pengd', 'wAtchmEn')) {
-// $firephp -> log('auth_passwd ');
-// echo "Authentication Successful!\n";
-// } else {
-// echo "Authentication Failed!\n";
-// $firephp -> log('die ');
-// die('Authentication Failed...');
-// }
-// $tunnel = ssh2_tunnel($session, 'alacritas.cis.utas.edu.au', 3307);
-// $firephp -> log('tunnel ');
-// $db = new mysqli('127.0.0.1', 'pengd', '207071', 'pengd', 3307, $tunnel);
-// $firephp -> log('db ');
-$mysqli = new mysqli("127.0.0.1", "root", "", "pengd");
-$firephp -> log('get mysqli');
+$mysqli = new mysqli("127.0.0.1", "pengd", "207071", "pengd");
+//$mysqli = new mysqli("127.0.0.1", "root", "", "pengd");
 if ($mysqli -> connect_error) {
 	die('Connect Error (' . $mysqli -> connect_errno . ') ' . $mysqli -> connect_error);
 }
-
-// echo 'Success ... ' . $mysqli -> hostinfo . "<br />";
 // echo phpinfo();
-
-// $server = 'DESKTOP1\SQLEXPRESS';
-// $db = mssql_connect($server, 'pengd', '207071', false);
-// if (! $db) {
-// 	die ( 'Failed to connect to MSSQL' );
-// }
 ?>
