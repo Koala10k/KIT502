19bc4b3b3463f6e2a1d135fdbccd3401bca4cb37
Koala10k <dupeng0217@gmail.com>
2014-04-26 17:02:12 +1000

    add feature: query db

--- survey.php
+++ survey.php
@@ -35,15 +35,15 @@
 	<div id="notice"></div>
 	<form id="surveyForm" method="post" action="">
 		Name:
-		<input type="text" name="name" autofocus="autofocus" pattern="[a-zA-Z_0-9]+" title="[a-zA-Z_0-9]+" required="true"/><br />
+		<input type="text" name="name" autofocus="autofocus" pattern="[a-zA-Z_0-9]+" title="[a-zA-Z_0-9]+" required/><br />
 		Email:
-		<input type="email" name="email" required="true"/><br />
+		<input type="email" name="email" required/><br />
 		Gender:
-		<input type="radio" name="gender" value="Male" required="true" />Male
+		<input type="radio" name="gender" value="Male" required />Male
 		<input type="radio" name="gender" value="Female" />Female
 		<br />
 		State:
-		<select name="state" required="true">
+		<select name="state" required>
 			<option value="" style="display:none">--please select an item--</option>
 			<option value="VIC">VIC</option>
 			<option value="NSW">NSW</option>
@@ -59,9 +59,36 @@
 		<input type="submit" name="sm" value="Submit" />
 		<!-- validate form with Regex -->
 	</form>
+	<div>
+	<form id="queryDB" method="post" action="">
+	<input type="submit" name="query" value="Query DB if you want :)" />
+	</form>
+<?php 
+	if(isset($_POST['query'])){
+	include ('db_conn.php');
+	$query = "select * from `survey`";
+	$result = $mysqli->query($query);
+	
+	echo "<table border='1'>";
+	echo "<tr><th>Name</th><th>Email</th><th>Gender</th><th>State</th></tr>";
+	while($row=$result->fetch_array(MYSQLI_ASSOC)){
+		$name = $row['Name'];
+		$email = $row['Email'];
+		$gender = $row['Gender'];
+		$state = $row['State'];
+		echo "<tr>";
+		echo "<td>$name</td>";
+		echo "<td>$email</td>";
+		echo "<td>$gender</td>";
+		echo "<td>$state</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+	}
+?>
+	</div>
 <?php
 if (isset($_POST['sm'])) {
-	// TODO validate from HTTP Header
 	include ('db_conn.php');
 	$name = $mysqli->real_escape_string($_POST['name']);
 	$email = $mysqli->real_escape_string($_POST['email']);
